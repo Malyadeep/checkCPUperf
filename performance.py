3d4e207cc5aff27b64320e96e48eb3f9fe29041f
Malyadeep <22d0136@iitb.ac.in>
2023-02-24 16:30:55 +0530

    Modified performance.py
    > 'performance.py' now takes sMin, sMax, no.of arrays and
    base of logspace command line arguments.
    > Added a main() function which parses command line arguments and
    calls the relevant functions.
    > Modified the try-exception block to allow user to force
    and overwrite previous results if present.
    > Modified the standard outputs of the program to provide
    more information and improve readibility.
    > All outputs are now saved in an 'output/' folder created by
    the program itself.

--- performance.py
+++ performance.py
@@ -2,7 +2,9 @@ import numpy as np
 import numba
 import matplotlib.pyplot as plt
 import time
+from argparse import ArgumentParser
 import flopFunc
+import os
 
 
 def make_data(N):
@@ -19,25 +21,27 @@ def axpb(y, x, a, b):
         y[i] = a[i] + b[i]*x[i]
 
 
-def bandWidthCompute():
+def bandWidthCompute(sMin, sMax, N, base, force):
+    totalInitialTime = time.perf_counter()
     try:
-        data = np.load('bw_N.npz')
-        sizes = data['sizes']
-        bandWidth = data['bandWidth']
-
-    except FileNotFoundError:
-        print('\nFile not found: running performance test for (bandWidth\
-             vs N)\n')
-        sMin = 1
-        sMax = 8
-        N = 25
-        base = 11
+        data = np.load('output/bw_N.npz')
+        if force is False:
+            print('\nPrevious result found --> replotting the results\n')
+            sizes = data['sizes']
+            bandWidth = data['bandWidth']
+        else:
+            print('\nforce option selected --> running test again\n')
+            print('WARNING! --> This will overwrite previous results')
+            raise Exception
+    except Exception:
+        if force is False:
+            print('\nNo previous results found: running performance test for' +
+                  ' peak bandwidth\n')
         sizes = np.logspace(sMin, sMax, N, base=base, dtype=int)
         bandWidth = np.zeros_like(sizes)
-        print(sizes)
         y, x, a, b = make_data(10)
-        print('\n\n-------------------- bandWidth vs N --------------------\n\
-            \n')
+        print('\n-------------------- bandWidth vs N --------------------\
+              \n')
         for itr in range(sizes.shape[0]):
             y, x, a, b = make_data(sizes[itr])
             runTimeTemp = []
@@ -58,8 +62,11 @@ def bandWidthCompute():
             runTime = np.min(np.array(runTimeTemp))
             bandWidth[itr] = (y.nbytes + x.nbytes + a.nbytes + b.nbytes)\
                 / runTime
-            print(sizes[itr], ', ', runTime, ', ', nLoop)
-        np.savez('bw_N.npz', bandWidth=bandWidth, sizes=sizes)
+            print(str(int(itr*100/sizes.shape[0])).ljust(2), '% done ',
+                  '| array size = ', str(sizes[itr]).ljust(12),
+                  '| run time = ', "{:.4e}".format(runTime),
+                  '| no.of iterations = ', str(nLoop).ljust(8))
+        np.savez('output/bw_N.npz', bandWidth=bandWidth, sizes=sizes)
 
     cpu_L1 = np.full((10,), fill_value=32*1024/8)
     cpu_L2_s = np.full((10,), fill_value=512*1024/8)
@@ -76,26 +83,37 @@ def bandWidthCompute():
     plt.legend(loc='best')
     plt.xlabel('size')
     plt.ylabel('Memory bandwidth (bytes/sec)')
-    plt.savefig('bandwidth_vs_N.png')
+    plt.savefig('output/bandwidth_vs_N.png')
+
+    totalRunTime = time.perf_counter() - totalInitialTime
+    print('\nBandwidth computation completed --> run time = ' +
+          "{:.4e}".format(totalRunTime) + ' --> peak bandwidth = ' +
+          "{:.4f}".format(np.max(bandWidth)/10**9) + ' GB/s \n')
+    print('\n##############################################################' +
+          '#########\n')
 
 
-def flopsCompute():
+def flopsCompute(sMin, sMax, N, base, force):
+    totalInitialTime = time.perf_counter()
     try:
-        data = np.load('flops_N.npz')
-        sizes = data['sizes']
-        flops = data['flops']
-    except FileNotFoundError:
-        print('\nFile not found: running performance test for (flops vs N)\n')
-        sMin = 1
-        sMax = 8
-        N = 25
-        base = 11
+        data = np.load('output/flops_N.npz')
+        if force is False:
+            print('\nPrevious result found --> replotting the results\n')
+            sizes = data['sizes']
+            flops = data['flops']
+        else:
+            print('\nforce option selected --> running test again\n')
+            print('WARNING! --> This will overwrite previous results')
+            raise Exception
+    except Exception:
+        if force is False:
+            print('\nNo previous results found: running performance test for' +
+                  ' peak FLOPS\n')
         sizes = np.logspace(sMin, sMax, N, base=base, dtype=int)
         flops = np.zeros((sizes.shape[0], 7))
-        print(sizes)
         for j in range(7):
-            print('\n\n-------------------- flops vs N ('+str(j)+')'
-                  '--------------------\n\n')
+            print('\n-------------------- flops vs N ('+str(j+1)+'/7)' +
+                  '--------------------\n')
             for itr in range(sizes.shape[0]):
                 y_test, x_test, a_test, b_test = make_data(5)
                 if j == 0:
@@ -144,8 +162,11 @@ def flopsCompute():
 
                 runTime = np.min(np.array(runTimeTemp))
                 flops[itr, j] = totFlops/runTime
-                print(sizes[itr], ', ', runTime, ', ', nLoop)
-        np.savez('flops_N.npz', flops=flops, sizes=sizes)
+                print(str(int(itr*100/sizes.shape[0])).ljust(2), '% done ',
+                      '| array size = ', str(sizes[itr]).ljust(12),
+                      '| run time = ', "{:.4e}".format(runTime),
+                      '| no.of iterations = ', str(nLoop).ljust(8))
+        np.savez('output/flops_N.npz', flops=flops, sizes=sizes)
 
     label = ['2 flop', '4 flop', '8 flop', '16 flop', '24 flop', '32 flop',
              '64 flop']
@@ -156,9 +177,43 @@ def flopsCompute():
     plt.legend(loc='best')
     plt.xlabel('size')
     plt.ylabel('FLOPS')
-    plt.savefig('flops_vs_N.png')
+    plt.savefig('output/flops_vs_N.png')
+
+    totalRunTime = time.perf_counter() - totalInitialTime
+    print('\nFLOPS computation completed --> run time = ' +
+          "{:.4e}".format(totalRunTime) + ' --> peak GFLOPS = ' +
+          "{:.4f}".format(np.max(flops)/10**9) + '\n')
+    print('\n##############################################################' +
+          '#########\n')
+
+
+def main():
+    startTime = time.perf_counter()
+    parser = ArgumentParser(description='Estimate peak bandwidth and GFLOPS ' +
+                            'of CPU')
+    parser.add_argument('--sMin', default=1, type=int,
+                        help='Min. order of array size')
+    parser.add_argument('--sMax', default=8, type=int,
+                        help='Max. order of array size')
+    parser.add_argument('--base', default=10, type=int,
+                        help='base of the log-scale to generate arrays ' +
+                        'of different sizes based on the minimum and ' +
+                        'maximum order')
+    parser.add_argument('-n', '--no_of_arrays', default=25, type=int,
+                        help='no.of arrays to generate')
+    parser.add_argument('-f', '--force', action='store_true', default=False,
+                        help='force to run the test again and ' +
+                        'overwrite previous results')
+    args = parser.parse_args()
+    if not os.path.exists('output'):
+        os.makedirs('output')
+    bandWidthCompute(args.sMin, args.sMax, args.no_of_arrays, args.base,
+                     args.force)
+    flopsCompute(args.sMin, args.sMax, args.no_of_arrays, args.base,
+                 args.force)
+    print('Run completed --> run time = ' + "{:.4e}".format(time.perf_counter()
+          - startTime) + '\n')
 
 
 if __name__ == '__main__':
-    bandWidthCompute()
-    flopsCompute()
+    main()
