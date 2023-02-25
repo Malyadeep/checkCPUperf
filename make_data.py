6e4fd37973f7c01999d5c32fa599b0b23bed4a8d
Malyadeep Bhattacharya <48013804+Malyadeep@users.noreply.github.com>
2023-02-25 11:57:40 +0530

    Delete make_data.py

--- make_data.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def make_data(N):
-    x = np.linspace(0, 2*np.pi, N)
-    a = np.linspace(0, np.pi, N)
-    b = np.linspace(0, np.pi/2, N)
-    y = np.zeros_like(x)
-    return y, x, a, b
